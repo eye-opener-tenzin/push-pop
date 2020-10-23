2a300e5ce5af60ffe2c007a3c5af634a23d01509
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-23 00:28:55 -0700

    add pusp, pop, shift, unshift file to github

---
+++ push-pop.js
@@ -0,0 +1,10 @@
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets.shift("The Moon");
+planets.push("Saturn");
+planets.unshift("Mercury");
+
+
+console.log(planets);
+document.write(planets);
